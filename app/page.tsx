6117995bc50d969672b2f1ca4a8b47f35abbbf06
Sanskar Kumar <106443271+mindspirex@users.noreply.github.com>
2024-08-21 00:09:20 +0530

    Add files via upload

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import Hello from "@/components/Hello";
+import Contact from "@/components/Contact";
+
+export default function Home() {
+  return (
+    <main>
+      <Hello />
+      <Contact />
+    </main>
+  );
+}
